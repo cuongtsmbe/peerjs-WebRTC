5e10fba53fe695589e4d9d44143c13abc5836400
Phan Huu Cuong <77524070+cuongtsmbe@users.noreply.github.com>
2021-06-23 23:35:14 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var exphbs = require('express-handlebars');
 var $ = require("jquery");
-var webpack = require("webpack");
+
 app.use(express.static("public"));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
